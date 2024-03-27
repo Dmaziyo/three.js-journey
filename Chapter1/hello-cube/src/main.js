ffcd429d7fd0f764a796fc959d6180cf28165e1c
liangjunqi <247948681@qq.com>
2024-03-27 11:55:38 +0800

    chapter1.1

---
+++ Chapter1/hello-cube/src/main.js
@@ -0,0 +1,46 @@
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from 'https://cdn.skypack.dev/three@0.149.0'
+
+// Get the reference to the scene-container
+const container = document.querySelector('#scene-container')
+
+// create the scene
+const scene = new Scene()
+
+// Set the bg color
+scene.background = new Color('skyblue')
+
+// Create a camera
+const fov = 35
+const aspect = container.clientWidth / container.clientHeight
+const near = 0.1
+const far = 100
+
+const camera = new PerspectiveCamera(fov, aspect, near, far)
+
+camera.position.set(0, 0, 10)
+
+const geometry = new BoxBufferGeometry(2, 2, 2)
+
+const material = new MeshBasicMaterial()
+
+const cube = new Mesh(geometry, material)
+
+scene.add(cube)
+
+const renderer = new WebGLRenderer()
+renderer.setSize(container.clientWidth, container.clientHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+
+renderer.render(scene,camera);
+
+container.append(renderer.domElement)
+
+
